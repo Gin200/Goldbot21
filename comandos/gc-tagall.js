078319a2e258783aa22fe317b7f6aba4605be083
Gin200 <144694376+Gin200@users.noreply.github.com>
2023-09-25 09:55:11 -0500

    Update gc-tagall.js

--- comandos/gc-tagall.js
+++ comandos/gc-tagall.js
@@ -4,11 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `*⺀DESPIERTEN BOTS NATYCHARBOT LOS INVOCA⺀*\n\n❏ ${oi}\n\n ETIQUETA GENERAL*\n`
+let oi = `*𝙼𝙅𝙐𝙀𝙂𝘼 𝙈𝙍𝘿𝘼:* ${pesan}`
+let teks = `*⺀𝘼𝙉𝙊𝙏𝘼𝙏𝙀 𝙈𝘼𝙉𝘾𝙓 𝘿𝙀 𝙈𝙄𝙀𝙍𝘿𝘼 ⺀*\n\n❏ ${oi}\n\n 𝙈𝘼𝙍𝙍𝙊𝙉𝙀𝙎*\n`
 for (let mem of participants) {
-teks += `•🐼  @${mem.id.split('@')[0]}\n`}
-teks += `*└Nati La Niña De Mis Ojos <3`
+teks += `☠️➭  @${mem.id.split('@')[0]}\n`}
+teks += `*└@goldarmy_mixto <3`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
